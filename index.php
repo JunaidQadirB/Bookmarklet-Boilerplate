07a6719666f83bad5a0a558d4e17b6207d02faf7
JeyKeu <shekahanzai.baloch@gmail.com>
2012-12-19 00:16:13 +0500

    InitializedProject

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <script>
+            (function() {
+                var myApp = document.createElement('script');
+                myApp.type = 'text/javascript';
+                myApp.async = true;
+                myApp.src = 'http://url/of/js/MyApp.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(myApp, s);
+            })();
+        </script>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ src/Bookmarklet.Boilerplate.js
@@ -0,0 +1,116 @@
+/**
+ * This is a Bookmarklet Boilerplate based on the Singleton Pattern.
+ * 
+ * @author Junaid Qadir Baloch (JeyKeu) <shekhanzai.baloch@gmail.com>
+ */
+var MyApp = (function() {
+    "use strict";
+    /*
+     * baseUrl - Url of the hosted app and resources that the bookmarklet loads
+     */
+    var baseUrl = 'http://';
+    /**
+     * getResource - Loads multiple Script and CSS resources fro the specified url.
+     * a call back is called when all the resources are loaded.
+     * @param string url, a string or a coma-separated list of urls
+     * @param string callback
+     * @param string type
+     */
+
+    function getResource(url, callback, type) {
+        var head = document.getElementsByTagName("head")[0],
+                done = false,
+                elem,
+                splitUrl = url.split(',');
+        type = type || 'script';
+
+        if (splitUrl.length > 0) {
+            for (var i = 0; i < splitUrl.length; i++) {
+                switch (type) {
+                    case 'script':
+                        {
+                            elem = document.createElement("script");
+                            elem.src = splitUrl[i];
+                            break;
+                        }
+                    case 'css':
+                        {
+                            elem = document.createElement("link");
+                            elem.type = "text/css";
+                            elem.rel = "stylesheet";
+                            elem.href = splitUrl[i];
+                            break;
+                        }
+                }
+
+                /* Attach handlers for all browsers*/
+                elem.onload = elem.onreadystatechange = function() {
+                    if (!done && (!this.readyState ||
+                            this.readyState === "loaded" || this.readyState === "complete")) {
+                        done = true;
+                        callback();
+                    }
+                };
+                head.appendChild(elem);
+            }
+        } else {
+            switch (type) {
+                case 'script':
+                    {
+                        elem = document.createElement("script");
+                        elem.src = url;
+                        break;
+                    }
+                case 'css':
+                    {
+                        elem = document.createElement("link");
+                        elem.type = "text/css";
+                        elem.rel = "stylesheet";
+                        elem.href = url;
+                        break;
+                    }
+            }
+            /* Attach handlers for all browsers
+             * 
+             * NOTE: css doesn't support ready states, but it worked for me.
+             * please do provide your feedback if you got a proper way to load css
+             */
+            elem.onload = elem.onreadystatechange = function() {
+                if (!done && (!this.readyState ||
+                        this.readyState === "loaded" || this.readyState === "complete")) {
+                    done = true;
+                    callback();
+                }
+            };
+            head.appendChild(elem);
+        }
+    }
+
+    /**
+     * Define your private functions herer if any intende
+     */
+    function privateFunction() {
+        alert("Hey! Well, I'm a private function.");
+    }
+
+    /**
+     * Load Resource from your server if any
+     */
+    getResource('http://' + baseUrl + '/any/js/I/want/to/load.js', function() {
+        /**
+         * The Boookmarklet is ready at this point
+         * Think of this place as the entry poit. Its lije the void main(void)
+         * of your bookmarklet.
+         * 
+         */
+        console.log('YAY! Songspk player has been loaded');
+    }, 'script');
+    return {
+        /**
+         * Define any public functions here
+         */
+        publicFunction: function() {
+            alert("I'm Public.Let me call the private function.");
+        }
+    };
+})();
